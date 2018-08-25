c87c6b44f3cad7dd39bcedca369b3c076c6b6dc5
Guido-V <42699863+Guido-V@users.noreply.github.com>
2018-08-25 12:13:26 -0500

    Update insert_libros.sql
    Guido Junior Valdez Sotelo

--- Biblio2018/Scripts/insert_libros.sql
+++ Biblio2018/Scripts/insert_libros.sql
@@ -11,11 +11,29 @@ Plantilla de script posterior a la implementación
 */
 INSERT dbo.Libro
         ( CodigoLibro, TituloLibro )
-VALUES  (  1, -- CodigoLibro - int
-          'Base de datos'  -- TituloLibro - varchar(150)
-          );
+VALUES  ( 1, 'Base de datos');
 INSERT dbo.Libro
         ( CodigoLibro, TituloLibro )
-VALUES  (  2, -- CodigoLibro - int
-          'Inteligencia de negocios'  -- TituloLibro - varchar(150)
-          );
+VALUES  ( 2, 'Inteligencia de negocios');
+INSERT dbo.Libro
+        ( CodigoLibro, TituloLibro )
+VALUES  ( 3, 'Base de Datos para Tontos');
+INSERT dbo.Libro
+        ( CodigoLibro, TituloLibro )
+VALUES  ( 4, 'Asu mare, el libro');
+INSERT dbo.Libro
+        ( CodigoLibro, TituloLibro )
+VALUES  ( 5, 'Fisica cuantica del papel mache');
+		-- Usuarios
+INSERT dbo.Usuario
+		( CodigoUsuario, NombreUsuario )
+VALUES	( 1, 'Admin' );
+INSERT dbo.Usuario
+		( CodigoUsuario, NombreUsuario )
+VALUES	( 2, 'Usuario' );
+		--Valoracion
+INSERT dbo.Valoración
+		( Usuario_CodigoUsuario, Libro_CodigoLibro, numerovaloracion, FechaValoracion, ValorValoracion )
+VALUES	( 1, 1, 4, GETDATE(), 4 );
+-- Guido Valdez Sotelo; parece que aun no estoy matricula en el Sistema T_T
+-- No enserio estoy perdido
