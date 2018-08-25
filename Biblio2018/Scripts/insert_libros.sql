a00a5fbc3cd300566e8d7d3b9be8ab6bfd6fd36c
p-cuadros <patrick_jcq@hotmail.com>
2018-08-25 11:10:16 -0500

    Agregar archivos de proyecto.

---
+++ Biblio2018/Scripts/insert_libros.sql
@@ -0,0 +1,21 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+INSERT dbo.Libro
+        ( CodigoLibro, TituloLibro )
+VALUES  (  1, -- CodigoLibro - int
+          'Base de datos'  -- TituloLibro - varchar(150)
+          );
+INSERT dbo.Libro
+        ( CodigoLibro, TituloLibro )
+VALUES  (  2, -- CodigoLibro - int
+          'Inteligencia de negocios'  -- TituloLibro - varchar(150)
+          );
---
+++ Biblio2018/dbo/Tables/Libro.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Libro] (
+    [CodigoLibro] INT           NOT NULL,
+    [TituloLibro] VARCHAR (150) NOT NULL,
+    CONSTRAINT [libro_pk] PRIMARY KEY CLUSTERED ([CodigoLibro] ASC)
+);
